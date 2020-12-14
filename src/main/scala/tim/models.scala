43f3ecfdda0bd83a55969d3604307cd8622b3462
S.H.Ayat <fahim.ayat@gmail.com>
2020-12-14 09:04:10 +0330

    initiating base models

--- src/main/scala/tim/models.scala
+++ src/main/scala/tim/models.scala
@@ -1,13 +1,46 @@
 package tim
 
-final case class ActivityMode(id: String, label: String, description: String, isDeprecated: Boolean)
+final case class Timestamp(value: String) extends AnyVal {
+  def parse: (Int, Int, Int) = {
+    val array = value.split(":").map(_.toInt)
+    (array(0), array(1), array(2))
+  }
 
-final case class Activity(id: String, label: String, modeId: String, description: String, isDeprecated: Boolean)
-
-sealed trait Record{
-  val start: Long
+  def hour: Int = parse._1
+  def minute: Int = parse._2
+  def second: Int = parse._3
 }
+
+// TODO better name
+final case class ActivityModeId(value: String) extends AnyVal
+final case class ActivityMode(
+    id: ActivityModeId,
+    label: String,
+    description: String,
+    isDeprecated: Boolean
+)
+
+final case class ActivityId(value: String) extends AnyVal
+final case class Activity(
+    id: ActivityId,
+    label: String,
+    modeId: ActivityModeId,
+    description: String,
+    isDeprecated: Boolean
+)
+
+sealed trait Record {
+  val start: Timestamp
+}
+
 object Record {
-final case class Simple(start: Long, end: Long, activityId: Option[String], description: String, issueId: Option[String])
-}
+  final case class Simple(
+      start: Timestamp,
+      end: Timestamp,
+      activityId: Option[String],
+      description: String,
+      issueId: Option[String]
+  ) extends Record
 
+  final case class EndOfDay(start: Timestamp) extends Record
+}
